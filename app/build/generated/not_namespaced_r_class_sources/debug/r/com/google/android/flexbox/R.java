e0eb59cb8006fe4c893514432ebb44bf56d57b2d
neo_tlaletsi <neo.tlaletsi@mailinator.com>
2019-11-22 20:06:19 +0200

    latest changes to implement facebook login authentication

--- app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/flexbox/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/com/google/android/flexbox/R.java
@@ -14,51 +14,51 @@ public final class R {
 
         public static final int alignContent = 0x7f030029;
         public static final int alignItems = 0x7f03002a;
-        public static final int dividerDrawable = 0x7f0300ea;
-        public static final int dividerDrawableHorizontal = 0x7f0300eb;
-        public static final int dividerDrawableVertical = 0x7f0300ec;
-        public static final int flexDirection = 0x7f030112;
-        public static final int flexWrap = 0x7f030113;
-        public static final int justifyContent = 0x7f030175;
-        public static final int layout_alignSelf = 0x7f03017b;
-        public static final int layout_flexBasisPercent = 0x7f0301ad;
-        public static final int layout_flexGrow = 0x7f0301ae;
-        public static final int layout_flexShrink = 0x7f0301af;
-        public static final int layout_maxHeight = 0x7f0301b8;
-        public static final int layout_maxWidth = 0x7f0301b9;
-        public static final int layout_minHeight = 0x7f0301ba;
-        public static final int layout_minWidth = 0x7f0301bb;
-        public static final int layout_order = 0x7f0301bd;
-        public static final int layout_wrapBefore = 0x7f0301c0;
-        public static final int maxLine = 0x7f0301d6;
-        public static final int showDivider = 0x7f03021b;
-        public static final int showDividerHorizontal = 0x7f03021c;
-        public static final int showDividerVertical = 0x7f03021d;
+        public static final int dividerDrawable = 0x7f030103;
+        public static final int dividerDrawableHorizontal = 0x7f030104;
+        public static final int dividerDrawableVertical = 0x7f030105;
+        public static final int flexDirection = 0x7f030130;
+        public static final int flexWrap = 0x7f030131;
+        public static final int justifyContent = 0x7f030195;
+        public static final int layout_alignSelf = 0x7f03019f;
+        public static final int layout_flexBasisPercent = 0x7f0301d1;
+        public static final int layout_flexGrow = 0x7f0301d2;
+        public static final int layout_flexShrink = 0x7f0301d3;
+        public static final int layout_maxHeight = 0x7f0301dc;
+        public static final int layout_maxWidth = 0x7f0301dd;
+        public static final int layout_minHeight = 0x7f0301de;
+        public static final int layout_minWidth = 0x7f0301df;
+        public static final int layout_order = 0x7f0301e1;
+        public static final int layout_wrapBefore = 0x7f0301e4;
+        public static final int maxLine = 0x7f0301fc;
+        public static final int showDivider = 0x7f030250;
+        public static final int showDividerHorizontal = 0x7f030251;
+        public static final int showDividerVertical = 0x7f030252;
     }
     public static final class id {
         private id() {}
 
-        public static final int auto = 0x7f080023;
-        public static final int baseline = 0x7f080025;
-        public static final int center = 0x7f080037;
-        public static final int column = 0x7f080040;
-        public static final int column_reverse = 0x7f080041;
-        public static final int flex_end = 0x7f08008d;
-        public static final int flex_start = 0x7f08008e;
-        public static final int nowrap = 0x7f0800f1;
-        public static final int row = 0x7f080103;
-        public static final int row_reverse = 0x7f080104;
-        public static final int space_around = 0x7f080126;
-        public static final int space_between = 0x7f080127;
-        public static final int space_evenly = 0x7f080128;
-        public static final int stretch = 0x7f080135;
-        public static final int wrap = 0x7f08017a;
-        public static final int wrap_reverse = 0x7f08017c;
+        public static final int auto = 0x7f080025;
+        public static final int baseline = 0x7f080028;
+        public static final int center = 0x7f08004b;
+        public static final int column = 0x7f080055;
+        public static final int column_reverse = 0x7f080056;
+        public static final int flex_end = 0x7f0800af;
+        public static final int flex_start = 0x7f0800b0;
+        public static final int nowrap = 0x7f080118;
+        public static final int row = 0x7f080138;
+        public static final int row_reverse = 0x7f080139;
+        public static final int space_around = 0x7f080162;
+        public static final int space_between = 0x7f080163;
+        public static final int space_evenly = 0x7f080164;
+        public static final int stretch = 0x7f080171;
+        public static final int wrap = 0x7f0801db;
+        public static final int wrap_reverse = 0x7f0801dd;
     }
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] FlexboxLayout = { 0x7f030029, 0x7f03002a, 0x7f0300ea, 0x7f0300eb, 0x7f0300ec, 0x7f030112, 0x7f030113, 0x7f030175, 0x7f0301d6, 0x7f03021b, 0x7f03021c, 0x7f03021d };
+        public static final int[] FlexboxLayout = { 0x7f030029, 0x7f03002a, 0x7f030103, 0x7f030104, 0x7f030105, 0x7f030130, 0x7f030131, 0x7f030195, 0x7f0301fc, 0x7f030250, 0x7f030251, 0x7f030252 };
         public static final int FlexboxLayout_alignContent = 0;
         public static final int FlexboxLayout_alignItems = 1;
         public static final int FlexboxLayout_dividerDrawable = 2;
@@ -71,7 +71,7 @@ public final class R {
         public static final int FlexboxLayout_showDivider = 9;
         public static final int FlexboxLayout_showDividerHorizontal = 10;
         public static final int FlexboxLayout_showDividerVertical = 11;
-        public static final int[] FlexboxLayout_Layout = { 0x7f03017b, 0x7f0301ad, 0x7f0301ae, 0x7f0301af, 0x7f0301b8, 0x7f0301b9, 0x7f0301ba, 0x7f0301bb, 0x7f0301bd, 0x7f0301c0 };
+        public static final int[] FlexboxLayout_Layout = { 0x7f03019f, 0x7f0301d1, 0x7f0301d2, 0x7f0301d3, 0x7f0301dc, 0x7f0301dd, 0x7f0301de, 0x7f0301df, 0x7f0301e1, 0x7f0301e4 };
         public static final int FlexboxLayout_Layout_layout_alignSelf = 0;
         public static final int FlexboxLayout_Layout_layout_flexBasisPercent = 1;
         public static final int FlexboxLayout_Layout_layout_flexGrow = 2;
--- app/src/main/java/com/gruv/LandingActivity.java
+++ app/src/main/java/com/gruv/LandingActivity.java
@@ -1,5 +1,9 @@
 package com.gruv;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 
@@ -7,12 +11,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
 
 import com.gruv.navigation.Navigation;
 
 public class LandingActivity extends AppCompatActivity {
-    protected static ConstraintLayout layoutLoginStart, layoutLoginEmail, layoutRegister, layoutForgotPassword, layoutEnterVerifyCode, layoutProgress, layoutResetPassword;
+    protected static ConstraintLayout
+            layoutLoginStart, layoutLoginEmail,
+            layoutRegister, layoutForgotPassword,
+            layoutEnterVerifyCode, layoutProgress,
+            layoutResetPassword;
+    ImageView imageFacebook;
+
+    private CallbackManager mCallbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,10 @@ public class LandingActivity extends AppCompatActivity {
         layoutEnterVerifyCode = (ConstraintLayout) findViewById(R.id.constraintLayoutEnterVerifyCode);
         layoutResetPassword = (ConstraintLayout)findViewById(R.id.constraintLayoutResetPassword);
         layoutProgress = (ConstraintLayout) findViewById(R.id.constraintLayoutProgressBar);
+
+
+        //login : social
+        imageFacebook = (ImageView) findViewById(R.id.imageFacebook);
 
 
         TextView textSignUp = (TextView) findViewById(R.id.textViewSignUp);
@@ -143,6 +162,33 @@ public class LandingActivity extends AppCompatActivity {
         }
     }
 
+   public void imgFacebookLogin(View  v){
+       Intent intent = new Intent(getApplicationContext(),FacebookLoginActivity.class);
+       startActivity(intent);
+    }
+
+    public void imgTwitterGenericLogin(View  v){
+        Intent intent = new Intent(getApplicationContext(),GenericIdpActivity.class);
+        startActivity(intent);
+    }
+    private  boolean connectionAvailable() {
+        boolean connected = false;
+        ConnectivityManager cm = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+
+        if (activeNetwork != null) {
+            if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) {
+                connected = true;
+            } else {
+                if (activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE) {
+                    connected = true;
+                } else {
+                    connected = false;
+                }
+            }
+        }
+        return connected;
+    }
 
 }
 
