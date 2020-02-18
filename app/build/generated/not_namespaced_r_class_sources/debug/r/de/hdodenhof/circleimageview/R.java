06ad0ee84e3fe2ec2585c4ef7a8f7651cf20eb97
sipphe <kambude@gmail.com>
2020-02-18 09:22:49 +0200

    day/night feature

--- app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/de/hdodenhof/circleimageview/R.java
@@ -12,15 +12,15 @@ public final class R {
     public static final class attr {
         private attr() {}
 
-        public static final int civ_border_color = 0x7f0300ab;
-        public static final int civ_border_overlay = 0x7f0300ac;
-        public static final int civ_border_width = 0x7f0300ad;
-        public static final int civ_circle_background_color = 0x7f0300ae;
+        public static final int civ_border_color = 0x7f0300ad;
+        public static final int civ_border_overlay = 0x7f0300ae;
+        public static final int civ_border_width = 0x7f0300af;
+        public static final int civ_circle_background_color = 0x7f0300b0;
     }
     public static final class styleable {
         private styleable() {}
 
-        public static final int[] CircleImageView = { 0x7f0300ab, 0x7f0300ac, 0x7f0300ad, 0x7f0300ae };
+        public static final int[] CircleImageView = { 0x7f0300ad, 0x7f0300ae, 0x7f0300af, 0x7f0300b0 };
         public static final int CircleImageView_civ_border_color = 0;
         public static final int CircleImageView_civ_border_overlay = 1;
         public static final int CircleImageView_civ_border_width = 2;
